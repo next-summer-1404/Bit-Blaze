4061f0640fcc59505a546e23df7556f69fa4f29f
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-10-02 00:59:55 +0330

    feat: implement categorysection ui

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${PeydaFanum.className} antialiased bg-[#232323] min-h-screen`}
+        className={`${PeydaFanum.className} antialiased bg-[#232323] min-h-screen overflow-x-hidden`}
       >
           <Header/>
           {children}
--- src/components/LandingPage/CategorySection/CategorySection.tsx
+++ src/components/LandingPage/CategorySection/CategorySection.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
-
+import React from "react";
+import { GoArrowLeft } from "react-icons/go";
 const CategorySection = () => {
-  return (
-    <div className='text-white'>CategorySection</div>
-  )
-}
+    return (
+        <div className="mt-32 mx-auto max-w-[98%] w-full flex flex-col justify-center items-center gap-5 relative">
+            <div className="flex flex-row gap-3 text-[#8CFF45] items-center font-[500] text-[16px]">
+                <svg
+                    width="48"
+                    height="16"
+                    viewBox="0 0 48 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M3 9.73205C1.66666 8.96225 1.66667 7.03775 3 6.26795L9 2.80385C10.3333 2.03405 12 2.9963 12 4.5359L12 11.4641C12 13.0037 10.3333 13.966 9 13.1962L3 9.73205Z"
+                        fill="#8CFF45"
+                    />
+                    <path
+                        d="M24.25 9.29904C23.25 8.72169 23.25 7.27831 24.25 6.70096L28.75 4.10289C29.75 3.52554 31 4.24722 31 5.40192L31 10.5981C31 11.7528 29.75 12.4745 28.75 11.8971L24.25 9.29904Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.5"
+                    />
+                    <path
+                        d="M41.5 8.86602C40.8333 8.48112 40.8333 7.51887 41.5 7.13397L44.5 5.40192C45.1667 5.01702 46 5.49815 46 6.26795L46 9.73205C46 10.5019 45.1667 10.983 44.5 10.5981L41.5 8.86602Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.25"
+                    />
+                    <path
+                        d="M3 9.73205C1.66666 8.96225 1.66667 7.03775 3 6.26795L9 2.80385C10.3333 2.03405 12 2.9963 12 4.5359L12 11.4641C12 13.0037 10.3333 13.966 9 13.1962L3 9.73205Z"
+                        fill="#8CFF45"
+                    />
+                    <path
+                        d="M24.25 9.29904C23.25 8.72169 23.25 7.27831 24.25 6.70096L28.75 4.10289C29.75 3.52554 31 4.24722 31 5.40192L31 10.5981C31 11.7528 29.75 12.4745 28.75 11.8971L24.25 9.29904Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.5"
+                    />
+                    <path
+                        d="M41.5 8.86602C40.8333 8.48112 40.8333 7.51887 41.5 7.13397L44.5 5.40192C45.1667 5.01702 46 5.49815 46 6.26795L46 9.73205C46 10.5019 45.1667 10.983 44.5 10.5981L41.5 8.86602Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.25"
+                    />
+                </svg>
+                دسته بندی املاک دلتا
+                <svg
+                    width="48"
+                    height="16"
+                    viewBox="0 0 48 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        d="M45 9.73205C46.3333 8.96225 46.3333 7.03775 45 6.26795L39 2.80385C37.6667 2.03405 36 2.9963 36 4.5359L36 11.4641C36 13.0037 37.6667 13.966 39 13.1962L45 9.73205Z"
+                        fill="#8CFF45"
+                    />
+                    <path
+                        d="M23.75 9.29904C24.75 8.72169 24.75 7.27831 23.75 6.70096L19.25 4.10289C18.25 3.52554 17 4.24722 17 5.40192L17 10.5981C17 11.7528 18.25 12.4745 19.25 11.8971L23.75 9.29904Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.5"
+                    />
+                    <path
+                        d="M6.5 8.86602C7.16667 8.48112 7.16667 7.51887 6.5 7.13397L3.5 5.40192C2.83333 5.01702 2 5.49815 2 6.26795L2 9.73205C2 10.5019 2.83333 10.983 3.5 10.5981L6.5 8.86602Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.25"
+                    />
+                    <path
+                        d="M45 9.73205C46.3333 8.96225 46.3333 7.03775 45 6.26795L39 2.80385C37.6667 2.03405 36 2.9963 36 4.5359L36 11.4641C36 13.0037 37.6667 13.966 39 13.1962L45 9.73205Z"
+                        fill="#8CFF45"
+                    />
+                    <path
+                        d="M23.75 9.29904C24.75 8.72169 24.75 7.27831 23.75 6.70096L19.25 4.10289C18.25 3.52554 17 4.24722 17 5.40192L17 10.5981C17 11.7528 18.25 12.4745 19.25 11.8971L23.75 9.29904Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.5"
+                    />
+                    <path
+                        d="M6.5 8.86602C7.16667 8.48112 7.16667 7.51887 6.5 7.13397L3.5 5.40192C2.83333 5.01702 2 5.49815 2 6.26795L2 9.73205C2 10.5019 2.83333 10.983 3.5 10.5981L6.5 8.86602Z"
+                        fill="#8CFF45"
+                        fillOpacity="0.25"
+                    />
+                </svg>
+            </div>
+            <h2 className="text-[#FFFFFF] text-[30px] font-[500]">
+                هر ملکی بخوای اینجا <b>پیدا میشه</b> !
+            </h2>
+            <p className="text-[#FFFFFF] text-[16px] font-[400]">
+                با کلیک به روی هر دسته بندی می‌توانید تمام آگهی مربوط آن را مشاهده کنید و به ملک مورد
+                علاقه خود برسید
+            </p>
+            <div className="w-full flex flex-row justify-center gap-5">
+                <div className="flex flex-col">
+                    <div className="flex flex-row gap-4 bg-[#2E2E2E] rounded-[14px] items-center "></div>
+                    <div className="flex flex-row gap-4 bg-[#2E2E2E] rounded-tl-[14px] rounded-bl-[14px] rounded-br-[14px] items-center relative before:content-[''] before:w-10 before:h-4 before:bg-[#2E2E2E] before:rounded-t-[11px] before:absolute before:right-[0px] before:top-[0px] before:-translate-y-4">
+                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 0L7.62054 4.37946L12 6L7.62054 7.62054L6 12L4.37946 7.62054L0 6L4.37946 4.37946L6 0Z" fill="white" />
+                        </svg>
+                        ملک ویلایی
+                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 0L7.62054 4.37946L12 6L7.62054 7.62054L6 12L4.37946 7.62054L0 6L4.37946 4.37946L6 0Z" fill="white" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div className="absolute left-[-98px] top-1/2 transform -translate-y-1/2 w-[218px] h-[218px] blur-[50px] bg-[#8CFF45]/15  rounded-full flex items-center justify-start p-4"></div>
+        </div>
+    );
+};
 
-export default CategorySection
+export default CategorySection;
