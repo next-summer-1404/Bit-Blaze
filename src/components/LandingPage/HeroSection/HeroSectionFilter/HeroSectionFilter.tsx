cf738280903d3434ad5c5dff93dbb0eb352872f3
Parsa Barzegar <parsabarzegar.88@gmail.com>
2025-10-13 21:04:13 +0330

    Update HeroSectionFilter.tsx
    Update HeroSectionFilter.tsx

--- src/components/LandingPage/HeroSection/HeroSectionFilter/HeroSectionFilter.tsx
+++ src/components/LandingPage/HeroSection/HeroSectionFilter/HeroSectionFilter.tsx
@@ -100,7 +100,6 @@ const HeroSectionFilter = () => {
         </div>
         
         <div className="flex flex-row items-center gap-5 p-5 w-full rounded-4xl bg-[#404040]">
-          {/* Combobox برای انتخاب استان */}
           <div className="min-w-[200px] w-full">
             <Popover open={openProvince} onOpenChange={setOpenProvince}>
               <PopoverTrigger asChild>
@@ -139,8 +138,6 @@ const HeroSectionFilter = () => {
               </PopoverContent>
             </Popover>
           </div>
-
-          {/* تقویم برای تاریخ ورود */}
           <div className="w-full">
             <Popover>
               <PopoverTrigger asChild>
@@ -171,7 +168,6 @@ const HeroSectionFilter = () => {
             </Popover>
           </div>
 
-          {/* تقویم برای تاریخ خروج */}
           <div className="w-full">
             <Popover>
               <PopoverTrigger asChild>
@@ -202,7 +198,6 @@ const HeroSectionFilter = () => {
             </Popover>
           </div>
 
-          {/* تعداد نفرات */}
           <div className="w-full">
             <input 
               className="w-full outline-0 text-[#AAAAAA] mr-2 bg-transparent border border-[#AAAAAA] rounded-md px-3 py-2"
